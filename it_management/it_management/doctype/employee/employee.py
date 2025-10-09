020c1a2e61ea3ab9786cad0b07c9fef24944ae80
Jeans K. Real <jkrb7@hotmail.com>
2025-10-09 14:42:54 -0600

    Adding Employee Role

--- it_management/it_management/doctype/employee/employee.py
+++ it_management/it_management/doctype/employee/employee.py
@@ -1,6 +1,4 @@
-import frappe
 from frappe.model.document import Document
-from it_management.it_management.doctype.employee_sim.employee_sim import EmployeeSIM
 
 
 class Employee(Document):
@@ -22,7 +20,7 @@ class Employee(Document):
 		branch: DF.Link
 		company: DF.Literal["Distribuidora Reyes NIC", "Distribuidora Reyes CR", "Transporte Reyes", "Kings Exports Imports", "Grupo Reyes"]
 		department: DF.Link
-		designation: DF.Literal["Abogado", "Analista", "Asistente Gerencia", "Auditor", "Auxiliar de Bodega", "Cajero", "Cajero\\Liquidador", "Cartera y Cobro", "Compras", "Contador", "Contador Rotativo", "Ejecutivo de Venta", "Importaciones", "Jefe de Area", "Liquidador", "Reponsable de Modulo", "Responsable de Bodega", "RRHH", "Soporte Tecnico", "Supervisor", "Supervisor de Ruta", "Supervisor de CEDI", "Supervisor de Modulo", "Caja y Facturacion", "Cajero", "Facturador", "Vendedor", "Vendedor en Ruta", "Mecanico Frio", "Mecanico General", "Mecanico Electrico", "Diseno Grafico"]
+		designation: DF.Literal["Abogado", "Analista", "Asistente Gerencia", "Auditor", "Auxiliar de Bodega", "Cajero", "Cajero y Facturador", "Cajero\\Liquidador", "Cartera y Cobro", "Vendedor", "Vendedor en Ruta", "Compras", "Contador", "Contador Rotativo", "Ejecutivo de Venta", "Importaciones", "Jefe de Area", "Liquidador", "Reponsable de Modulo", "Responsable de Bodega", "RRHH", "Soporte Tecnico", "Supervisor", "Supervisor de Ruta", "Supervisor de CEDI", "Supervisor de Modulo", "Mecanico Frio", "Mecanico General", "Mecanico Electrico", "Diseno Grafico"]
 		first_name: DF.Data
 		gender: DF.Link | None
 		last_name: DF.Data
@@ -31,9 +29,4 @@ class Employee(Document):
 		personal_phone: DF.Phone | None
 		reports_to: DF.Link | None
 	# end: auto-generated types
-
-	def validate(self):
-		employee_sims = frappe.db.get_list("EmployeeSIM")
-
-		for device in self.assigned_sims:
-			pass
+	pass
